cde105197c2fbd3ec95ced96dc644d6dfeb1464f
Mogilisetti Sreeteja <mogilisettisreeteja@Mogilisettis-MacBook-Pro-2.local>
2023-11-12 00:29:32 +0530

    Version 2

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module db.com/m
 
 go 1.21.3
 
-require github.com/go-sql-driver/mysql v1.7.1
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/gorilla/mux v1.8.1
+)
--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 // mysql> select * from score;
@@ -46,16 +49,45 @@ import (
 // +-------+------+-----------+
 // 3 rows in set (0.01 sec)
 
+var db *sql.DB
+
+type ScoreTable struct {
+	Country string  `json: Country`
+	Overs   float32 `json: Overs`
+	Runs    int     `json: Runs`
+}
+
 func main() {
 	// mysql -u root -p mysqlpass
 	fmt.Println("This is my first mysql")
+
+	r := mux.NewRouter()
+	//Read API
+	r.HandleFunc("/data/{country}", handleGetWithQueryParam).Methods("GET")
+	//Delete API
+	r.HandleFunc("/data/{country}", handlerDelete).Methods("DELETE")
+	//Create API
+	r.HandleFunc("/data/", handlerPostWithRequestBody).Methods("POST")
+	// Update API
+	r.HandleFunc("/data/{country}/", handleUpdate).Methods("PUT")
+
+	http.Handle("/", r)
+
+	http.ListenAndServe(":8089", nil)
 
 	db, err := sql.Open("mysql", "root:mysqlpass@tcp(localhost:3306)/cricket")
 	if err != nil {
 		log.Fatal(err)
 	}
-	rows, err := db.Query("SELECT * FROM score")
-	fmt.Println("This is Teju db")
+	defer db.Close()
+}
+
+func handleGetWithQueryParam(w http.ResponseWriter, r *http.Request) {
+	QueryParams := r.URL.Query()
+
+	name := QueryParams.Get("country")
+
+	rows, err := db.Query("SELECT * FROM score where country=", name)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -70,7 +102,57 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+		fmt.Println("Get details of country: ", name)
 		fmt.Println(country, Runs, Overs)
 	}
-	defer db.Close()
+
+}
+
+func handleUpdate(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handlerDelete(w http.ResponseWriter, r *http.Request) {
+
+	QueryParams := r.URL.Query()
+
+	name := QueryParams.Get("country")
+
+	// Use a prepared statement to insert the value
+	stmt, err := db.Prepare("DELETE FROM score where country=?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	// Execute the prepared statement with the variable value
+	_, err = stmt.Exec(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Deleted details of country: ", name)
+
+}
+
+func handlerPostWithRequestBody(w http.ResponseWriter, r *http.Request) {
+	var details ScoreTable
+	err := json.NewDecoder(r.Body).Decode(details)
+	if err != nil {
+		http.Error(w, "Failed to decode request body", http.StatusBadRequest)
+		return
+	}
+	// Use a prepared statement to insert the value
+	stmt, err := db.Prepare("INSERT INTO score (Overs, Runs, country) VALUES (?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	// Execute the prepared statement with the variable value
+	_, err = stmt.Exec(details.Overs, details.Runs, details.Country)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
